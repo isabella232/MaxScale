349473e1d2103d4fdaaca25755e2618f848f896c
Markus Makela <markus.makela@mariadb.com>
2016-10-09 10:00:58 +0300

    Log more precise DCB write error messages
    The DCB error messages now log the type of the DCB and the remote address
    in addition to the system error message. The file descriptor and memory
    address are no longer printed in the error message as they are not useful
    to the end user. The fd and address are now logged at debug level with a
    more verbose error message.

--- server/modules/protocol/MySQL/MySQLBackend/mysql_backend.c
+++ server/modules/protocol/MySQL/MySQLBackend/mysql_backend.c
@@ -1215,12 +1215,6 @@ static int gw_backend_hangup(DCB *dcb)
         /* dcb_close(dcb); */
         goto retblock;
     }
-#if defined(SS_DEBUG)
-    if (ses_state != SESSION_STATE_STOPPING)
-    {
-        MXS_ERROR("Backend hangup error handling.");
-    }
-#endif
 
     router->handleError(router_instance,
                         rsession,
@@ -1233,9 +1227,6 @@ static int gw_backend_hangup(DCB *dcb)
     /** There are no required backends available, close session. */
     if (!succp)
     {
-#if defined(SS_DEBUG)
-        MXS_ERROR("Backend hangup -> closing session.");
-#endif
         spinlock_acquire(&session->ses_lock);
         session->state = SESSION_STATE_STOPPING;
         spinlock_release(&session->ses_lock);
